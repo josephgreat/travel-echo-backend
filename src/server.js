cf2cec0f65c92c710760cb5dd30085723a25662f
Adedero <118962395+Adedero@users.noreply.github.com>
2025-03-13 09:44:16 +0100

    Create http server

--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
+const http = require('node:http')
 const env = require('#utils/env')
 const logger = require('#utils/logger')
 const { IS_PRODUCTION_ENV } = require('#utils/constants')
 const PORT = env.get('PORT', 5000)
 const app = require('./app')
 
-app.listen(PORT, () => {
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
   logger.info(
     `Server running on ${IS_PRODUCTION_ENV ? `port ${PORT}` : `http://localhost:${PORT}`}`
   )
 })
 
 process.on('SIGINT', () => {
-  app.close(() => {
+  server.close(() => {
     logger.info('Server closed.')
     process.exit(0)
   })
